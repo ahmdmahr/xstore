a74f546583aeb33dbdda4ec15c864e753715047b
Ahamd Maher <ahmadmaher.work@gmail.com>
2024-09-19 18:28:19 +0300

    Integrate admin panel theme

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/home', [HomeController::class, 'index'])->name('home');
 Route::group(['middleware'=>['auth','isAdmin']],function(){
 
     Route::get('/dashboard',function(){
-        return view('admin.dashboard');
+        return view('admin.index');
     });
     
 });
