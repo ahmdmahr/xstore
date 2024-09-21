e7c8647339d05ce587de6417d57993fa87b032e3
Ahamd Maher <ahmadmaher.work@gmail.com>
2024-09-22 00:04:33 +0300

    Fetch featured products through frontend

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -7,6 +7,26 @@
 @section('content')
 
    @include('layouts.frontend.slider')
+
+
+  <div class="py-5">
+    <div class="container">
+      <div class="row">
+        @foreach ($featured_products as $prod)
+          
+        <div class="col-md-3 mt-3">
+          <div class="card">
+           <img src="{{asset('assets/uploads/product/'.$prod->image)}}" alt="Product Image">
+           <div class="card-body">
+             <h5> {{$prod->name}} </h5>
+             <small>{{$prod->selling_price}}$</small>
+           </div>
+          </div>
+        </div>
+        @endforeach
+
+      </div>
+    </div>
+  </div>
    
-  <h1>Welcome</h1>
 @endsection
